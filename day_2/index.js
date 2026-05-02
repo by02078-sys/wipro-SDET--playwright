29fbd9ba2b3f6266790d69ad02ff31b8059bf2cc
Balu <balu@Balus-MacBook-Air.local>
2026-05-02 10:07:05 +0530

    arrow fun and filter

--- day_2/index.js
+++ day_2/index.js
@@ -4,3 +4,11 @@ let c="akhi";
 //console.log("hello " + a + ","+b+"and"+c+"!");
 let d='hello ${a}, ${b} and ${c}!';
 console.log(d);
+
+let sum = (x, y) => x + y; 
+console.log("Sum of 5 and 10:", sum(5, 10)); 
+
+let arr = [1, 2, 3, 4, 5, "balu"];
+
+
+arr.filter(x => typeof x === "number").map(y => y * 2).forEach(abc => console.log("Element after * 2:", abc * 2));

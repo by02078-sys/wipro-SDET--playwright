195acaf1114b513a0bac9fe77a2cb2db5400cd9c
Balu <balu@Balus-MacBook-Air.local>
2026-05-13 14:40:50 +0530

    problem2

--- day_11/problem1.ts
+++ day_11/problem1.ts
@@ -1,3 +1,8 @@
+//1. The Generic API Wrapper
+//Scenario: You need a reusable function to fetch data that automatically types the response.
+//
+// Task: Write a generic function fetchData<T>(url: string): Promise<T>. It should use the fetch API, check if the response is okay, and return the JSON parsed as type T. Test it by creating an Album interface and fetching data from a mock URL.
+
 async function fetchData<T>(url: string): Promise<T> {
     const response = await fetch(url);
     if(!response.ok) {
